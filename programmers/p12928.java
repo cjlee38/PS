004b242d10f71cb99032a15b8ab63f9a13f3b482
cjlee38 <gptpem38@gmail.com>
2020-09-29 22:37:32 +0900

    SOLVE p12928

---
+++ programmers/p12928.java
@@ -0,0 +1,20 @@
+package programmers;
+
+// 프로그래머스 - 약수의 합
+public class p12928 {
+    public static void main(String[] args) {
+        int n = 12;
+        int result = solution(n);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
