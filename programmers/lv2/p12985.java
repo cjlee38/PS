38b70f0319216501b9944cb4f6a84a8931c9da8a
cjlee <gptpem38@gmail.com>
2020-10-26 04:13:13 +0900

    SOLVE p12985

---
+++ programmers/lv2/p12985.java
@@ -0,0 +1,27 @@
+package programmers.lv2;
+
+public class p12985 {
+    public static void main(String[] args) {
+        int n = 8;
+        int a = 3;
+        int b = 5;
+
+
+        System.out.println(solution(n, a, b));
+        System.out.println(solution2(n, a, b));
+    }
+
+    public static int solution(int n, int a, int b) {
+        return Integer.toBinaryString(a^b).length();
+    }
+
+    public static int solution2(int n, int a, int b) {
+        int answer = 0;
+        while (a != b) {
+            b = b % 2 == 1 ? b / 2 + 1 : b / 2;
+            a = a % 2 == 1 ? a / 2 + 1 : a / 2;
+            answer++;
+        }
+        return answer;
+    }
+}
