bda5fa8fbebe3af425b158bb456f95870af4874b
cjlee <gptpem38@gmail.com>
2020-11-01 11:37:31 +0900

    SOLVE p12949

---
+++ programmers/lv2/p12949.java
@@ -0,0 +1,31 @@
+package programmers.lv2;
+
+public class p12949 {
+    public static void main(String[] args) {
+        int[][] arr1 = {
+                {1, 4},
+                {3, 2},
+                {4, 1}
+        };
+        int[][] arr2 = {
+                {3, 3},
+                {3, 3}
+        };
+
+        int[][] result = solution(arr1, arr2);
+    }
+
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
