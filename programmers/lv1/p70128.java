29a3c9cc851a0a65ca0449ef9e0f93ac97174a28
cjlee <gptpem38@gmail.com>
2020-11-15 22:39:55 +0900

    SOLVE p70128

---
+++ programmers/lv1/p70128.java
@@ -0,0 +1,13 @@
+package programmers.lv1;
+
+public class p70128 {
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+}
