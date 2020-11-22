5ad428c471c05f127f10f75225fb776c820aad59
cjlee <gptpem38@gmail.com>
2020-11-22 13:37:12 +0900

    RESOLVE p12923

--- programmers/lv4/p12923.java
+++ programmers/lv4/p12923.java
@@ -1,15 +1,25 @@
 package programmers.lv4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class p12923 {
     public static void main(String[] args) {
         long begin = 990000000;
-        long end = 990000010;
+        long end = 990010000;
 
+        List<Long> list = new ArrayList<>();
         p12923 p = new p12923();
-        int[] answer = p.solution(begin, end);
-        for (int i : answer) {
-            System.out.print(i + " ");
+        for (int i = 0; i < 10; i++) {
+            long s = System.currentTimeMillis();
+            int[] answer = p.solution(begin, end);
+            long e = System.currentTimeMillis();
+
+            list.add(e - s);
         }
+
+        double elapsed = list.stream().mapToLong(Long::longValue).average().getAsDouble();
+        System.out.println("elapsed = " + elapsed);
     }
 
     public int[] solution(long begin, long end) {
@@ -17,21 +27,21 @@ public class p12923 {
         int[] road = new int[n];
 
         for (int i = 0; i < n; i++) {
-            long cur = i + begin;
-            road[i] = getGCD(cur);
+            int cur = (int) (begin + i);
+            road[i] = getGD(cur);
         }
 
         if (begin == 1) road[0] = 0;
         return road;
     }
 
-    private int getGCD(long cur) {
-        for (long i = 2; i <= Math.sqrt(cur); i++) {
-            if (cur / i > 10000000) continue;
-            if (cur % i == 0) return (int) (cur / i);
+    private int getGD(int cur) {
+        for (int i = 2; i <= Math.sqrt(cur); i++) {
+            if (cur % i == 0 && (cur / i) < 10000000) {
+                return cur / i;
+            }
         }
 
         return 1;
     }
-
 }
