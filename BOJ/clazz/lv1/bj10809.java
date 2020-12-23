6775b1cf478f3c4fefdfa927196fa828ff23d9bc
cjlee <gptpem38@gmail.com>
2020-12-23 20:39:58 +0900

    SOLVE : bj10809 in BOJ

--- BOJ/clazz/lv1/bj10809.java
+++ BOJ/clazz/lv1/bj10809.java
@@ -1,17 +1,35 @@
 package BOJ.clazz.lv1;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * uri =
- * name =
+ * uri = https://www.acmicpc.net/problem/10809
+ * name = 알파벳 찾기
  */
 
 public class bj10809 {
+    private final static int ASCII = 97;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+
+        int[] alphas = new int[26];
+        Arrays.fill(alphas, -1);
+
+        for (int i = 0; i < str.length(); i++) {
+            int pos = str.charAt(i) - ASCII;
+            if (alphas[pos] == -1) alphas[pos] = i;
+        }
+
+        for (int i = 0; i < alphas.length; i++) {
+            bw.write(alphas[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
