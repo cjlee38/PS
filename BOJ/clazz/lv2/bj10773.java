facd5b6ef72cff816623bccbb26fd7c417982b38
cjlee <gptpem38@gmail.com>
2020-12-26 07:38:57 +0900

    SOLVE : bj10773 IN BOJ

--- BOJ/clazz/lv2/bj10773.java
+++ BOJ/clazz/lv2/bj10773.java
@@ -1,30 +1,29 @@
 package BOJ.clazz.lv2;
 
 import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
- * uri = https://www.acmicpc.net/problem/
- * name =
+ * uri = https://www.acmicpc.net/problem/10773
+ * name = 제로
  */
 
 public class bj10773 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
 
-
-        bw.flush();
-        bw.close();
-    }
-
-    static class Solution {
-
-
-        public int run() {
-
-            return -1;
+        Stack<Integer> st = new Stack<>();
+        while(k-- > 0) {
+            int n = sc.nextInt();
+            if (n == 0) st.pop();
+            else st.push(n);
         }
+
+        int sum = st.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+
     }
 }
