a9b8a04ce1387b7a3861fadc4f76aef719169ec9
cjlee <gptpem38@gmail.com>
2020-12-26 10:19:54 +0900

    SOLVE : bj11651 in BOJ

--- BOJ/clazz/lv2/bj11651.java
+++ BOJ/clazz/lv2/bj11651.java
@@ -1,4 +1,36 @@
 package BOJ.clazz.lv2;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * uri = https://www.acmicpc.net/problem/11650
+ * name = 좌표 정렬하기 2
+ */
+
 public class bj11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> o1[1] == o2[1] ? (o1[0] - o2[0]) : (o1[1] - o2[1]));
+
+        for (int[] rr : arr) {
+            bw.write(rr[0] + " " + rr[1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
 }
