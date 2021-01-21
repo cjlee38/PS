2a0c7992c45916885a4c3745ee901ca7f7f0f1dd
cjlee <gptpem38@gmail.com>
2021-01-21 11:06:28 +0900

    SOLVE : bj11660 IN BOJ

---
+++ BOJ/clazz/lv4/bj11660.java
@@ -0,0 +1,59 @@
+package BOJ.clazz.lv4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * uri = https://www.acmicpc.net/problem/11660
+ * name = 구간 합 구하기 5
+ * tier = silver 1
+ */
+
+public class bj11660 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = getDP(n, arr);
+        while(m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            bw.write(getSum(dp, x1, y1, x2, y2) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int[][] getDP(int n, int[][] arr) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        return dp;
+    }
+
+    private static int getSum(int[][] dp, int x1, int y1, int x2, int y2) {
+        return dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+    }
+}
