ae5057d08cba468d8fcf5c12748c2eb6ad009f86
cjlee38 <gptpem38@gmail.com>
2021-08-03 11:36:58 +0900

    SOLVE : Shift-Reverse-String in Codetree

---
+++ src/main/java/codetree/intermediate/low/basic/ShiftReverseString.java
@@ -0,0 +1,36 @@
+package codetree.intermediate.low.basic;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ShiftReverseString {
+
+    static class Main {
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            while (n-- > 0) {
+                int command = Integer.parseInt(br.readLine());
+                if (command == 1) {
+                    sb.append(sb.charAt(0));
+                    sb.deleteCharAt(0);
+                }
+                else if (command == 2) {
+                    sb.insert(0, sb.charAt(sb.length() - 1));
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                else if (command == 3)
+                    sb.reverse();
+                System.out.println(sb.toString());
+            }
+
+            bw.flush();
+            bw.close();
+        }
+    }
+}
