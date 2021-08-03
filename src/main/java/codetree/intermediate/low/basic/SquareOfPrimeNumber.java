4da5c711e3dc8b8458800c6c7413ef1f25cfbaa3
cjlee38 <gptpem38@gmail.com>
2021-08-03 22:27:38 +0900

    SOLVE : Square-Of-Prime-Number

--- src/main/java/codetree/intermediate/low/basic/SquareOfPrimeNumber.java
+++ src/main/java/codetree/intermediate/low/basic/SquareOfPrimeNumber.java
@@ -1,6 +1,7 @@
 package codetree.intermediate.low.basic;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class SquareOfPrimeNumber {
 
@@ -10,8 +11,28 @@ public class SquareOfPrimeNumber {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int cnt = 0;
+            for (int i = start; i <= end; i++) {
+                int numOfDivisor = getDivisor(i);
+                if (numOfDivisor == 3)
+                    cnt++;
+            }
+            bw.write(cnt + "");
             bw.flush();
             bw.close();
+        }
+
+        private static int getDivisor(int v) {
+            int cnt = 0;
+            for (int i = 1; i <= v; i++) {
+                if (v % i == 0)
+                    ++cnt;
+            }
+            return cnt;
         }
     }
 }
