e255a571680b28c80f66af4779ed0c8e741e57b9
cjlee38 <gptpem38@gmail.com>
2021-08-03 22:33:21 +0900

    SOLVE : Filp-Integer in Codetree

---
+++ src/main/java/codetree/intermediate/low/basic/FlipInteger.java
@@ -0,0 +1,20 @@
+package codetree.intermediate.low.basic;
+
+import java.io.*;
+
+public class FlipInteger {
+
+    static class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int n = Integer.parseInt(br.readLine());
+            int r = Integer.parseInt(new StringBuilder(String.valueOf(n)).reverse().toString());
+            bw.write(r + "");
+
+            bw.flush();
+            bw.close();
+        }
+    }
+}
