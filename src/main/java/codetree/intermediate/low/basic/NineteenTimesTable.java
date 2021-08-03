01731684ef53d17435c6ec627db6896423ea0ba8
cjlee38 <gptpem38@gmail.com>
2021-08-03 10:34:16 +0900

    SOLVE : Nineteen-Times-Table in Codetree

---
+++ src/main/java/codetree/intermediate/low/basic/NineteenTimesTable.java
@@ -0,0 +1,28 @@
+package codetree.intermediate.low.basic;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class NineteenTimesTable {
+
+    static class Main {
+
+        private static final int FINAL_NUMBER = 19;
+
+        public static void main(String[] args) throws IOException {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            for (int i = 1; i <= FINAL_NUMBER; i++) {
+                for (int j = 1; j <= FINAL_NUMBER; j++) {
+                    bw.write(i + " * " + j + " = " + i * j);
+                    bw.write(j % 2 != 0 && j != FINAL_NUMBER ? " / " : "\n");
+                }
+            }
+
+            bw.flush();
+            bw.close();
+        }
+    }
+
+
+}
