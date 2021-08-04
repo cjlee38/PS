c7a238402fb50d5a9137df2c0dbdea966caf54e6
cjlee38 <gptpem38@gmail.com>
2021-08-04 11:19:41 +0900

    SOLVE : Max-Of-Unique-Number in Codetree

---
+++ src/main/java/codetree/intermediate/low/basic/MaxOfUniqueNumber.java
@@ -0,0 +1,34 @@
+package codetree.intermediate.low.basic;
+
+import java.io.*;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
+
+public class MaxOfUniqueNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Long> map = Arrays.stream(arr)
+                .boxed()
+                .collect(groupingBy(x -> x, counting()));
+        Integer key = map.entrySet().stream()
+                .filter((en) -> en.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .max(Comparator.naturalOrder())
+                .orElse(-1);
+
+        bw.write(key + "");
+
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/main/java/codetree/intermediate/low/basic/TwoMaxOfNNum.java
@@ -0,0 +1,30 @@
+package codetree.intermediate.low.basic;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class TwoMaxOfNNum {
+    static class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            bw.write(arr[arr.length - 1] + " " + arr[arr.length - 2]);
+
+
+
+
+            bw.flush();
+            bw.close();
+        }
+    }
+}
