09a79c6f0a5d6990b3bb75a785bfa204f39274d0
cjlee38 <gptpem38@gmail.com>
2021-08-04 10:50:18 +0900

    SOLVE : Look-And-Say-Sequence in Codetree

---
+++ src/main/java/codetree/intermediate/low/basic/LookAndSaySequence.java
@@ -0,0 +1,40 @@
+package codetree.intermediate.low.basic;
+
+import java.io.*;
+
+public class LookAndSaySequence {
+    static class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int n = Integer.parseInt(br.readLine()) - 1;
+            String s = "1";
+            while (n-- > 0)
+                s = getNextSequence(s);
+            bw.write(s);
+
+            bw.flush();
+            bw.close();
+        }
+
+        private static String getNextSequence(String s) {
+            StringBuilder ret = new StringBuilder();
+            char cur = s.charAt(0);
+            int cnt = 0;
+            for (char c : s.toCharArray()) {
+                if (c == cur)
+                    cnt++;
+                else {
+                    ret.append("" + cur + cnt);
+                    cnt = 1;
+                    cur = c;
+                }
+            }
+            if (cnt != 0)
+                ret.append("" + cur + cnt);
+
+            return ret.toString();
+        }
+    }
+}
