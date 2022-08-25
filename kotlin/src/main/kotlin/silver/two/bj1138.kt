b2dbf35bd9f25e7b031e6df52d3cf7c0361fcbfb
cjlee38 <cjlee38@github.com>
2022-08-25 18:07:29 +0900

    solve: bj1138

---
+++ kotlin/src/main/kotlin/silver/two/bj1138.kt
@@ -0,0 +1,21 @@
+package silver.two
+
+fun main() {
+    val N = readln().toInt()
+    val lefts = readln().split(" ").map { it.toInt() }
+
+    val line = (0 until N).map { 0 }.toMutableList()
+
+    for ((num, left) in lefts.withIndex()) {
+        var life = left
+        for (i in 0 until line.size) {
+            if (life == 0 && line[i] == 0) {
+                line[i] = num + 1
+                break
+            }
+            if (line[i] == 0) life--
+        }
+    }
+
+    println(line.joinToString(" "))
+}
