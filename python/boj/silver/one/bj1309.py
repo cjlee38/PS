17373862bc0e591037ff03ff1d8edb6526b81b22
cjlee38 <gptpem38@gmail.com>
2022-12-29 18:28:55 +0900

    solve: bj1309 in boj

---
+++ python/boj/silver/one/bj1309.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * max(2, N + 1)
+dp[0] = 1
+dp[1] = 3
+
+for i in range(2, N + 1):
+    dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901
+print(dp[-1])
+
